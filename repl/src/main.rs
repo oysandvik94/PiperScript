8d0082cc76df08a22d06dd06273cbbce175a5d07
Øystein Sandvik <oysandvik94@gmail.com>
2024-05-02 20:34:18 +0200

    First draft for repl
    Just retuns lexed tokens from input

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::stdin;
+
+use lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to lasagnelang, try and write some code");
+
+    loop {
+        let mut buffer = String::new();
+
+        match stdin().read_line(&mut buffer) {
+            Ok(_) => {
+                let name = buffer.trim_end();
+                let res = {
+                    let lexer = Lexer::new(name);
+                    lexer.generate_tokens()
+                };
+
+                println!("{:?}", res);
+            }
+            Err(_) => panic!(),
+        }
+    }
 }
