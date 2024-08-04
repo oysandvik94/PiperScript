0c557828168027d2dca2063bf60d131a45d53da7
Øystein Sandvik <oysandvik94@gmail.com>
2024-08-04 16:21:30 +0200

    remove son of anton

--- crates/son_of_anton/src/handlers/completion.rs
+++
@@ -1,8 +0,0 @@
-use tower_lsp::lsp_types::{CompletionItem, CompletionParams, CompletionResponse};
-
-pub fn handle_completion(_: CompletionParams) -> CompletionResponse {
-    CompletionResponse::Array(vec![
-        CompletionItem::new_simple("Hello".to_string(), "Some detail".to_string()),
-        CompletionItem::new_simple("Bye".to_string(), "More detail".to_string()),
-    ])
-}
--- crates/son_of_anton/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod capabilites;
-
-pub mod handlers {
-    pub mod completion;
-    pub mod didchange;
-    pub mod documentopen;
-    pub mod gotodefinition;
-    pub mod hover;
-}
